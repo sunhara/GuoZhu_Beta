36cb2c416be431d623523304684a50a796da1d3e
sunhara <Tom_gaoshen@163.com>
2023-04-23 19:24:21 +0800

    update

--- GuoZhu_Beta.tab/General.panel/Stiffener_Volume.pushbutton/script.py
+++ GuoZhu_Beta.tab/General.panel/Stiffener_Volume.pushbutton/script.py
@@ -1,8 +1,10 @@
 # dependencies
+# -*- coding: utf-8 -*-
 import clr
 clr.AddReference('System.Windows.Forms')
 clr.AddReference('IronPython.Wpf')
 
+import Autodesk
 from Autodesk.Revit.DB import*
 
 doc = __revit__.ActiveUIDocument.Document
@@ -22,12 +24,21 @@ t.Start()
 
 for i in all_stiffeners:
     targetVolume = i.LookupParameter("Stiffener_Volume")
-    volume = i.LookupParameter("Volume")
-    value = round(volume.AsDouble(),5)
-    Para.append(value)
-    
-    targetVolume.Set(value)
 
+    if  doc.Application.Language.ToString() == 'English_USA':
+        #Or using Autodesk.Revit.ApplicationServices.LanguageType.English_USA
+        volume = i.LookupParameter("Volume")
+        value = round(volume.AsDouble(),5)
+        Para.append(value)
+
+        targetVolume.Set(value)
+
+    else:
+        volume = i.LookupParameter("体积")
+        value = round(volume.AsDouble(),5)
+        Para.append(value)
+    
+        targetVolume.Set(value)
 
 
 

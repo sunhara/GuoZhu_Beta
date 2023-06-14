e9399ffb278ac04421ea4ad1f508da682718ef92
sunhara <Tom_gaoshen@163.com>
2023-06-14 14:36:54 +0800

    update

--- GuoZhu_Beta.tab/Architectural&Interior.panel/Materials_Match.pulldown/load_materials.pushbutton/script.py
+++ GuoZhu_Beta.tab/Architectural&Interior.panel/Materials_Match.pulldown/load_materials.pushbutton/script.py
@@ -30,26 +30,24 @@ fam_doc = app.OpenDocumentFile(file_path)
 #Source materials
 materials = FilteredElementCollector(fam_doc).OfCategory(BuiltInCategory.OST_Materials).WhereElementIsNotElementType()
 matNames = [e.Name for e in materials]
-matIds = [e.Id for e in materials]
 
 #Destination materials
 doc_materials = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Materials).WhereElementIsNotElementType()
 doc_matNames = [e.Name for e in doc_materials]
-doc_matIds = [e.Id for e in doc_materials]
 
 #filtering out the difference
 x = set(doc_matNames)
 y = set(matNames)
-same = list(y.intersection(x))
+different = list(y.difference(x))
 
 common_eles = []
 for element in materials:
-    if element.Name in same:
+    if element.Name in different:
         common_eles.append(element)
 
 
 options = [ElementToCopy(e) for e in common_eles]
-elesToCopy = forms.SelectFromList.show(options, title = "选择材质同步", width = 500, button_name = "同步", multiselect = True)
+elesToCopy = forms.SelectFromList.show(options, title = "选择材质导入", width = 500, button_name = "导入", multiselect = True)
 #Script exict point
 if not elesToCopy:
     fam_doc.Close(False)
@@ -63,30 +61,12 @@ copyOpts = CopyPasteOptions()
 trans = Transform.Identity
 
 
-#filtering out the materials to delete
-matNamesDel = [e.Name for e in elesToCopy]
-xDel = set(matNamesDel)
-yDel = set(matNames)
-same = list(yDel.intersection(xDel))
 
-dele_eles = []
-for element in doc_materials:
-    if element.Name in same:
-        dele_eles.append(element)
+t = Transaction(doc,"Copy Elements")
+t.Start()
 
-#Delete Element 
-ids_del = [e.Id for e in dele_eles]
-ids_ToDel= List[ElementId](ids_del)
+ElementTransformUtils.CopyElements(fam_doc,ids_copy,doc,trans,copyOpts)
 
-res = forms.alert("同步材质会移除当前材质，需要从新赋予材质！",ok = True)
-if res:
-    t = Transaction(doc,"Copy Elements")
-    t.Start()
+t.Commit()
 
-    doc.Delete(ids_ToDel) 
-
-    ElementTransformUtils.CopyElements(fam_doc,ids_copy,doc,trans,copyOpts)
-
-    t.Commit()
-
-    fam_doc.Close(False)
+fam_doc.Close(False)
--- GuoZhu_Beta.tab/General.panel/Testing.pushbutton/script.py
+++ GuoZhu_Beta.tab/General.panel/Testing.pushbutton/script.py
@@ -1,8 +1,3 @@
 # -*- coding: utf-8 -*-
 from pyrevit import forms	
 forms.alert("可以干活了", exitscript=True)
-
-	
-    
-
-

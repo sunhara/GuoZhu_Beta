a758e91ded07faf2ed9922a0dd02754f42f2105d
sunhara <Tom_gaoshen@163.com>
2023-06-21 15:20:48 +0800

    modified

--- GuoZhu_Beta.tab/General.panel/Stiffener_Volume.pushbutton/script.py
+++ GuoZhu_Beta.tab/General.panel/Stiffener_Volume.pushbutton/script.py
@@ -1,48 +1,78 @@
 # dependencies
 # -*- coding: utf-8 -*-
 import clr
-clr.AddReference('System.Windows.Forms')
-clr.AddReference('IronPython.Wpf')
-
 import Autodesk
 from Autodesk.Revit.DB import*
 
 doc = __revit__.ActiveUIDocument.Document
-
+app = __revit__.Application
 
 # Creating collector instance and collecting all the stiffeners from the model
 all_stiffeners = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StructuralStiffener).WhereElementIsNotElementType()
 
-t = Transaction(doc,"This is a new transaction")
+#Stiffener category
+stiff_cate = Category.GetCategory(doc,BuiltInCategory.OST_StructuralStiffener)
+cateSet = doc.Application.Create.NewCategorySet()
+cateSet.Insert(stiff_cate)
 
+#Define the shared parameter file path
+shared_para_fp = "\\\\10.1.37.5\\国住共享文件夹\\国住设计区\\设计共享区\\BIM项目\\共享参数模板（Shared Parameters）\\Shared Parameters-2023.txt"
+
+#Set the Condition
+condition = all_stiffeners.ToElements()[0].LookupParameter("Stiffener_Volume")
 
 Para = []
 
+# Condition for whether it needs for adding a shared parameters
+if condition == None or condition.IsReadOnly:
 
-t.Start()
+    #Create a new shared parameter file object
+    app.SharedParametersFilename = shared_para_fp
+    spFile = doc.Application.OpenSharedParameterFile()
+    defGroups = spFile.Groups
 
+    for dg in defGroups:
+    #groupNames.append(dg.Name)
 
-for i in all_stiffeners:
-    targetVolume = i.LookupParameter("Stiffener_Volume")
+        if dg.Name == "钢板-Stiffener":
+            
+            exterianl_def = [d for d in dg.Definitions]
 
-    if  doc.Application.Language.ToString() == 'English_USA':
-        #Or using Autodesk.Revit.ApplicationServices.LanguageType.English_USA
-        volume = i.LookupParameter("Volume")
+            for d in exterianl_def:
+                if d.Name == "Stiffener_Volume":
+                    
+                    with Transaction(doc) as t:    
+                        t.Start("Add Shared Parameters and set value")
+                        #Binding the parameter to category 
+                        newIB = doc.Application.Create.NewInstanceBinding(cateSet)
+                        #Binding map insert
+                        doc.ParameterBindings.Insert(d,newIB,BuiltInParameterGroup.PG_ANALYSIS_RESULTS)
+
+                        #Convert Externaldefinition to internalDefinition in order to SetAllowVaryBetweenGroups!
+                        internal_sp = SharedParameterElement.Lookup( doc,d.GUID)
+                        internal_sp.GetDefinition().SetAllowVaryBetweenGroups(doc,True)
+
+                        for i in all_stiffeners:
+                            targetVolume = i.LookupParameter("Stiffener_Volume")
+                            #compute family instance volume
+                            volume = i.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED)
+                            value = round(volume.AsDouble(),5)
+                            Para.append(value)
+                            #set to the variable
+                            targetVolume.Set(value)
+
+                        t.Commit()
+else:
+    t = Transaction(doc,"This is a new transaction")
+
+    t.Start()
+    for i in all_stiffeners:
+        targetVolume = i.LookupParameter("Stiffener_Volume")
+        #compute family instance volume
+        volume = i.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED)
         value = round(volume.AsDouble(),5)
         Para.append(value)
-
+        #set to the variable
         targetVolume.Set(value)
 
-    else:
-        volume = i.LookupParameter("体积")
-        value = round(volume.AsDouble(),5)
-        Para.append(value)
-    
-        targetVolume.Set(value)
-
-
-
-t.Commit()
-
-print(Para)
-
+    t.Commit()
--- GuoZhu_Beta.tab/Selection.panel/query.pushbutton/script.py
+++ GuoZhu_Beta.tab/Selection.panel/query.pushbutton/script.py
@@ -2,6 +2,7 @@
 import clr
 import json
 
+
 clr.AddReference('System')
 from System.Collections.Generic import List
 
@@ -13,64 +14,23 @@ from Autodesk.Revit.DB import*
 from Autodesk.Revit.ApplicationServices import*
 
 
-doc = __revit__.ActiveUIDocument.Document
-app = __revit__.Application
 
-active_view = doc.ActiveView
-uiapp = UIApplication(doc.Application)
-
-wall_cate = Category.GetCategory(doc,BuiltInCategory.OST_Walls)
-roof_cate = Category.GetCategory(doc,BuiltInCategory.OST_Roofs)
-floor_cate = Category.GetCategory(doc,BuiltInCategory.OST_Floors)
-stiff_cate = Category.GetCategory(doc,BuiltInCategory.OST_StructuralStiffener)
-frame_cate = Category.GetCategory(doc,BuiltInCategory.OST_StructuralFraming)
-col_cate = Category.GetCategory(doc,BuiltInCategory.OST_StructuralColumns)
-conn_cate = Category.GetCategory(doc,BuiltInCategory.OST_StructConnections)
-tempstr_cate = Category.GetCategory(doc,BuiltInCategory.OST_TemporaryStructure)
+doc = __revit__
+doc1 = Autodesk.Revit.UI.ExternalCommandData.Application
+app = UIApplication()
+doc2 = app.ActiveUIDocument.Document
 
 
-cateSet = doc.Application.Create.NewCategorySet()
-cateSet.Insert(wall_cate)
-cateSet.Insert(roof_cate)
-cateSet.Insert(stiff_cate)
-cateSet.Insert(frame_cate)
-cateSet.Insert(col_cate)
-cateSet.Insert(conn_cate)
-cateSet.Insert(tempstr_cate)
-cateSet.Insert(floor_cate)
+
+#app = __revit__.Application
 
 
-#Define the shared parameter file path
-shared_para_fp = "\\\\10.1.37.5\\国住共享文件夹\\国住设计区\\设计共享区\\BIM项目\\共享参数模板（Shared Parameters）\\Shared Parameters-2023.txt"
-
-#Create a new shared parameter file object
-app.SharedParametersFilename = shared_para_fp
-spFile = doc.Application.OpenSharedParameterFile()
-defGroups = spFile.Groups
-
-groupNames = []
-for dg in defGroups:
-    groupNames.append(dg.Name)
-    if dg.Name == "工厂加工":
-        
-        exterianl_def = [d for d in dg.Definitions]
-
-        for d in exterianl_def:
-            if d.Name == "工厂加工-零件标号":
-                
-                with Transaction(doc) as t:    
-                    t.Start("Add Shared Parameters")
-                    #Binding the parameter to category 
-                    newIB = doc.Application.Create.NewInstanceBinding(cateSet)
-                    #Binding map insert
-                    doc.ParameterBindings.Insert(d,newIB,BuiltInParameterGroup.PG_IDENTITY_DATA)
-
-                    #Convert Externaldefinition to internalDefinition in order to SetAllowVaryBetweenGroups!
-                    internal_sp = SharedParameterElement.Lookup( doc,d.GUID)
-                    internal_sp.GetDefinition().SetAllowVaryBetweenGroups(doc,True)
-                    t.Commit()
+# active_view = doc.ActiveView
+# uiapp = UIApplication(doc.Application)
 
 
-# print(catSet)
+print(doc)
+print(doc2)
+
 # print("--"*50)
 # print(json.dumps(groupNames,encoding ='utf-8',ensure_ascii=False))

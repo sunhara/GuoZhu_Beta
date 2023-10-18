99fd833cd19177b838889e414d6d655850ff43a5
sunhara <Tom_gaoshen@163.com>
2023-10-18 13:48:04 +0800

    update

--- GuoZhu_Beta.tab/General.panel/Lookup.pulldown/StructureMass.pushbutton/script.py
+++ GuoZhu_Beta.tab/General.panel/Lookup.pulldown/StructureMass.pushbutton/script.py
@@ -13,17 +13,41 @@ from Autodesk.Revit.UI import*
 from Autodesk.Revit.DB import*
 from Autodesk.Revit.ApplicationServices import*
 
+def SubCompon(famIns):
+    
+    global all_selected_elements
+
+    subCompIds = famIns.GetSubComponentIds()
+
+    if len(subCompIds) == 0:
+        
+        all_selected_elements.append(famIns)
+    else:
+        for i in subCompIds:
+            all_selected_elements.append(doc.GetElement(i))
+
+
+
+# function calculate all element's mass     
+def CalculateMass(element):
+
+    vol = element.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED).AsDouble()
+    vol_m3 = UnitUtils.ConvertFromInternalUnits(vol,UnitTypeId.CubicMeters)
+    global totalweight
+    totalweight = totalweight+vol_m3*7890
+
 
 doc = __revit__.ActiveUIDocument.Document
 app = __revit__.Application
 uidoc =__revit__.ActiveUIDocument
 
-active_view = doc.ActiveView
-uiapp = UIApplication(doc.Application)
 
 model_selected = [doc.GetElement(e_id) for e_id in uidoc.Selection.GetElementIds()]
 
 totalweight = 0
+
+all_selected_elements = []
+
 
 for i in model_selected:
 
@@ -31,15 +55,17 @@ for i in model_selected:
     if i.ToString() == "Autodesk.Revit.DB.Group":
 
         eles = i.GetMemberIds()
-
+        
         for e in eles:
             element = doc.GetElement(e)
-            vol = element.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED).AsDouble()
-            vol_m3 = UnitUtils.ConvertFromInternalUnits(vol,UnitTypeId.CubicMeters)
-            totalweight = totalweight+vol_m3*7890
-    else:
-        vol = i.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED).AsDouble()
-        vol_m3 = UnitUtils.ConvertFromInternalUnits(vol,UnitTypeId.CubicMeters)
-        totalweight = totalweight+vol_m3*7890
+            SubCompon(element)
 
+    else:
+        
+        SubCompon(i)
+        
+
+uniqueElements = list(set([i.Id for i in all_selected_elements]))
+
+[CalculateMass(doc.GetElement(e)) for e in uniqueElements]
 forms.alert('所选钢结构物体总质量：{} kg'.format(totalweight), exitscript=True)    
--- GuoZhu_Beta.tab/Selection.panel/query.pushbutton/script.py
+++ GuoZhu_Beta.tab/Selection.panel/query.pushbutton/script.py
@@ -18,53 +18,21 @@ class ElementToCopy(forms.TemplateListItem):
         return self.get_Parameter(BuiltInParameter.SYMBOL_NAME_PARAM).AsString()
 
 
+def SubCompon(famIns):
+    
+    subCompIds = famIns.GetSubComponentIds()
+    if len(subCompIds) == 0:
+        return famIns
+    else:
+        return [doc.GetElement(i) for i in subCompIds]
+
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
 selected_ID = uidoc.Selection.GetElementIds()
 
+famInstance = doc.GetElement(selected_ID[0])
 
-#Check if the initial elements has been selected
-if len(selected_ID) != 0:
-    pass
-else:
-    script.exit()
+obj = famInstance.GetMemberIds()
 
-# Collect all family symbol
-allFamily = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_GenericModel).WhereElementIsElementType().ToElements()
-
-# Selection popup
-options = [ElementToCopy(e) for e in allFamily]
-famSymbol = forms.SelectFromList.show(options, title = "选择材质导入", width = 500, button_name = "导入", multiselect = True)
-
-print(famSymbol)
-
-#Check if the copy elements has been selected
-if famSymbol != None:
-    pass
-else:
-    script.exit()
-
-
-
-for i in selected_ID:
-   
-
-    element = doc.GetElement(i)
-
-    location = element.Location
-
-    hostface = element.HostFace
-
-    startXYZ = element.Location.Point
-
-    endXYZ = element.FacingOrientation
-
-    t = Transaction(doc,"Start a new Transaction")
-    t.Start()
-
-    doc.Create.NewFamilyInstance(hostface,startXYZ,endXYZ,famSymbol[0])
-     
-    # doc.Create.NewFamilyInstance.Overloads[Reference, XYZ, XYZ, FamilySymbol](hostface,startXYZ,endXYZ,famSymbol[0])
-     
-    t.Commit() 
+print(obj)

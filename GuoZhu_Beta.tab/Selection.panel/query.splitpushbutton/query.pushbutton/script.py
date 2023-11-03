a861e1db2782ed0e80e522b6d4d53700b0d0cb5c
sunhara <Tom_gaoshen@163.com>
2023-11-03 15:46:23 +0800

    new tool

---
+++ GuoZhu_Beta.tab/Selection.panel/query.splitpushbutton/query.pushbutton/script.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import clr
+
+import json
+
+clr.AddReference('System')
+from System.Collections.Generic import List
+
+
+import Autodesk
+from Autodesk.Revit.DB import*
+
+from pyrevit import forms,script
+
+doc = __revit__.ActiveUIDocument.Document
+app = __revit__.Application
+
+# Creating collector instance and collecting all the stiffeners from the model
+collector = FilteredElementCollector(doc).OfClass(ImportInstance).ToElements()
+
+for e in collector:
+
+    print(e.Category.Name)
+    print("链接--{}".format(e.IsLinked))
+    viewname = doc.GetElement(e.OwnerViewId).Name
+    print(viewname)
+
+    
+
+# print(json.dumps(elem.Name,encoding ='utf-8',ensure_ascii=False))
+t = Transaction(doc, "purge")
+t.Start()
+
+for e in collector:
+    if e.IsLinked == True:
+        pass
+    else:
+        doc.Delete(e.Id)
+
+
+t.Commit()

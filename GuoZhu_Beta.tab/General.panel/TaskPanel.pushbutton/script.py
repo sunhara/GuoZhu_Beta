1d5b2f8bbc78ebe93ac6e721e79e8342ab71631d
sunhara <Tom_gaoshen@163.com>
2023-11-22 09:56:35 +0800

    task panel tool

--- GuoZhu_Beta.tab/General.panel/TaskPanel.pushbutton/script.py
+++ GuoZhu_Beta.tab/General.panel/TaskPanel.pushbutton/script.py
@@ -20,11 +20,11 @@ from Autodesk.Revit.DB import*
 
 doc = __revit__.ActiveUIDocument.Document
 
-#xmal_file = script.get_bundle_file("ui.xaml")
-xmal_file = "C:\\Users\\6321011\\Desktop\\firstWPFApp\\firstWPFApp\\firstWPFApp\\ui.xaml"
+xmal_file = script.get_bundle_file("ui.xaml")
+# xmal_file = "C:\\Users\\6321011\\Desktop\\firstWPFApp\\firstWPFApp\\firstWPFApp\\ui.xaml"
 # The task log location
 task_log = "\\\\10.1.37.5\\国住共享文件夹\\国住设计区\\设计共享区\\BIM项目\\pyRevitExtension\\worklog\\task_log.txt"
-
+task_log_completed = "\\\\10.1.37.5\\国住共享文件夹\\国住设计区\\设计共享区\\BIM项目\\pyRevitExtension\\worklog\\task_log_completed.txt"
 
 
 # function for unique user names
@@ -36,12 +36,9 @@ for i in all_3d_view:
 
 user_names = list(set(user_names))
 
+
+
 # List of tasks
-
-#function for extract index list from a list
-def Extract(lst,i):
-    return [item[i] for item in lst]
-
 if task_log is not None:
     #Modified the csv data list
     modified_f = []
@@ -55,14 +52,35 @@ if task_log is not None:
                 # extract all pirority levels
                 piror_level.append(row[0].split(":")[0])
 
+# List of completed tasks
+if task_log_completed is not None:
+    #Modified the csv data list
+    modified_f_c = []
+
+    if task_log_completed:
+        with codecs.open(task_log_completed, 'r', encoding="UTF-8") as f:
+            
+            for row in csv.reader(f):
+                # extract all strings
+                modified_f_c.append(','.join(row))
+
+
+# remove the completed tasks
+for i,j in zip(modified_f,piror_level):
+    if i in modified_f_c:
+        modified_f.remove(i)
+        piror_level.remove(j)
+
 
 # The item in the ScrollViewer
-class CheckBoxListItem:
-    def __init__(self, contex, piror_level, isChecked=False):
+class CheckBoxListItem():
+    def __init__(self, context, piror_level, index,isChecked=False):
         
-        self.Contex = contex
+        self.Context = context
         self.Pl = piror_level
+        self.index = index
         self.IsChecked = isChecked
+
 
 
 # convert into object
@@ -71,23 +89,27 @@ listViewData = []
 for name,level in zip(modified_f,piror_level):
 
     if level == "高":
-        item = CheckBoxListItem(name,"Red")
+        item = CheckBoxListItem(name,"Red",1)
         listViewData.append(item)
 
+
     elif level == "中":
-        item = CheckBoxListItem(name,"Orange")
+        item = CheckBoxListItem(name,"Orange",2)
         listViewData.append(item)
 
     elif level == "低":
-        item = CheckBoxListItem(name,"YellowGreen")
+        item = CheckBoxListItem(name,"YellowGreen",3)
         listViewData.append(item)
 
 
+#sorted all the context by their pirority level index
+newlistViewData = sorted(listViewData,key =lambda x: x.index)
 
 
 class ViewModelBase(INotifyPropertyChanged):
     def __init__(self):
         self.propertyChangedHandlers = []
+
 
     def RaisePropertyChanged(self, propertyName):
         args = PropertyChangedEventArgs(propertyName)
@@ -101,17 +123,14 @@ class ViewModelBase(INotifyPropertyChanged):
         self.propertyChangedHandlers.remove(handler)
 
 
-
-
-
 class MyWindow(Windows.Window):
     
 
     def __init__(self):
         wpf.LoadComponent(self,xmal_file)
         
-        self.assignTo.ItemsSource = user_names #self.vs.vsnames
-        self.ListBox1.ItemsSource = listViewData
+        self.assignTo.ItemsSource = user_names 
+        self.ListBox1.ItemsSource = newlistViewData
         
 
 
@@ -133,9 +152,14 @@ class MyWindow(Windows.Window):
     def complete(self,sender,args):
         
         # The Content.Name is the ComboBox Item's Grid's Name
-        selected_task = self.VisualTreeHelper.GetParent
-        print(selected_task)
-
+        selected_task = self.ListBox1.ItemsSource
+        for i in selected_task:
+            if i.IsChecked:
+                f = codecs.open(task_log_completed,"a",encoding = "utf-8")
+                f.write("{}\n".format(i.Context))
+                f.close
+                self.Close()
+        
 
 # MyWindow().Show()
 MyWindow().ShowDialog()

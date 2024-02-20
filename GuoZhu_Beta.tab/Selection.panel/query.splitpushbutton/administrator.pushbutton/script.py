b652abe7eba81544299a2b12419303df9b70985e
sunhara <Tom_gaoshen@163.com>
2024-02-20 13:58:25 +0800

    new admin tool

--- GuoZhu_Beta.tab/Selection.panel/query.splitpushbutton/administrator.pushbutton/script.py
+++ GuoZhu_Beta.tab/Selection.panel/query.splitpushbutton/administrator.pushbutton/script.py
@@ -2,130 +2,156 @@
 import clr
 import xml.etree.ElementTree as ET
 
-clr.AddReference('System')
-from System.Collections.Generic import List
-from Autodesk.Revit.UI import*
 
-import Autodesk
+from pyrevit import forms, script
+
 from Autodesk.Revit.DB import*
-
-from pyrevit import forms,script
+from Autodesk.Revit.UI.Selection import*
 
 
 doc = __revit__.ActiveUIDocument.Document
-app = __revit__.Application
 uidoc = __revit__.ActiveUIDocument
 
-app_username = app.Username
+def tunning(newRoot):
+    print("****"*10)
+    for rt in newRoot:
+        user = rt.get("name")
+        print("<{}>".format(user))
+        admin = rt.find("admin").text
+        print("\t admin-{}".format(admin))
+        workset = rt.find("workset").text
+        print("\t workset-{}".format(workset))
 
 
-
-# xmal_file = script.get_bundle_file("ui.xaml")
-# xmal_file = "C:\\Users\\6321011\\Desktop\\firstWPFApp\\firstWPFApp\\firstWPFApp\\ui.xaml"
+output = script.get_output()
 
 #Public config and local config
 public_config = "\\\\10.1.37.5\\国住共享文件夹\\国住设计区\\设计共享区\\BIM项目\\pyRevitExtension\\GUOZHU_Beta.extension\\GUOZHU_Beta_config.xml"
+text_XML = "C:\\Users\\6321011\\Desktop\\textXML.xml"
 
-XML_user_names = []
+
+# The main code check for admin
+try:
+    value = forms.ask_for_string(
+        
+        prompt='INPUT ADMIN CODE:',
+        title='Admin Check'
+    )
+
+    print(value)
+    if value == "1q2w3e4r":
+        print("correct")
+    else:
+        forms.alert("错误，请联系管理员")
+        output.close()
+        script.exit()
+except:
+    script.exit()
+
+
+# parse the public config(original) from the utf-8 xml
 tree = ET.parse(public_config)
 root = tree.getroot()
-
-target_user = []
-for user in root.findall("user"):
-
-    if user.get("name") == app_username:
-        target_user.append(user)
+# Create new root
+newRoot = ET.Element("data")
 
 
-try: 
+for rt in root:
 
-    if target_user[0].find("workset").text=="True":
-        print("true")
-    else:
-        print("false1")
+    user = rt.get("name").decode("utf-8")
+    admin = rt.find("admin").text
+    workset = rt.find("workset").text
+
+    #Create new root
+    newElem = ET.Element("user", name = user)
+
+    subE1 = ET.SubElement(newElem, "admin")
+    subE1.text = admin
+    subE2 = ET.SubElement(newElem, "workset")
+    subE2.text = workset
+
+    # root.append(newElem)
+    newRoot.append(newElem)
+
+newTree = ET.ElementTree(newRoot)
+
+# running cmd command
+exit_condition = False
+while exit_condition == False:
+    #user input command
     
-except:
-    print("false2")
+    while True:
+        tunning(newRoot)
+        users = [i.get("name") for i in newRoot.findall("user")]
+        cmd = raw_input()
 
-for i in root:
-    value = i.attrib['name']
-    # Input context with utf-8 must decode with utf-8
-    XML_user_names.append(value.decode("utf-8"))
+        if cmd.lower() == "rf":
+            exit_condition = True
+            break
 
-print(app_username)
+        #cmd for individual user
+        if len(cmd.split("_"))==2:
 
+            for user in newRoot.findall("user"):
+                input_name = cmd.split("_")[0]
+                input_cmd1 = cmd.split("_")[1]
+                
+                if user.get("name") == input_name:
+                    if input_cmd1.lower() == "workset.true" :
+                        user.find("workset").text = "True"
 
+                    elif input_cmd1.lower() == "workset.false":
+                        user.find("workset").text = "False"
 
+                    elif input_cmd1.lower() == "admin.true":
+                        user.find("admin").text = "True"
 
+                    elif input_cmd1.lower() == "admin.false":
+                        user.find("admin").text = "False"
 
+            exit_condition = False
+            break
 
+        if cmd.lower() == "admin.alltrue":
+            # set all user's admin to ture
+            for i in newRoot.iter("admin"):
+                i.text = "True"
+            exit_condition = False
+            break
 
+        if cmd.lower() == "admin.allfalse":
+            # set all user's admin to False
+            for i in newRoot.iter("admin"):
+                i.text = "False"
+            exit_condition = False
+            break
 
+        if cmd.lower() == "workset.alltrue":
+            # set all user's workset to ture
+            for i in newRoot.iter("workset"):
+                i.text = "True"
+            exit_condition = False
+            break
 
+        if cmd.lower() == "workset.allfalse":
+            # set all user's workset to false
+            for i in newRoot.iter("workset"):
+                i.text = "False"
+            exit_condition = False
+            break
 
+        if cmd.lower() == "exit":
+            # exit with nothing changed
+            exit_condition = True
+            output.close()
+            break
 
+        if cmd.lower() == "write":
+            # writ to xml and exit
+            newTree.write(public_config, encoding="UTF-8",xml_declaration=True)
+            exit_condition = True
+            break
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# The main code
-# try:
-#     value = forms.ask_for_string(
-        
-#         prompt='INPUT ADMIN CODE:',
-#         title='Admin Check'
-#     )
-
-#     print(value)
-#     if value == "1q2w3e4r":
-#         print("correct")
-#     else:
-#         forms.alert("错误，请联系管理员")
-# except:
-#     script.exit()
-
-
-
-# print(json.dumps(elem.Name,encoding ='utf-8',ensure_ascii=False))
-# t = Transaction(doc, "purge")
-# t.Start()
-
-# for e in collector:
-#     if e.IsLinked == True:
-#         pass
-#     else:
-#         doc.Delete(e.Id)
-
-
-# t.Commit()
-
-# elements = FilteredElementCollector(doc).WhereElementIsNotElementType().ToElementIds()
-
-# selected_ID = uidoc.Selection.GetElementIds()
-
-# selected_eles = [doc.GetElement(i).Name for i in selected_ID]
-
-
-# print("total elements")
-# print(len(elements))
-# print("selected types")
-# out1 = list(set(selected_eles))
-# print(len(out1))
-
-
-# CmndID = RevitCommandId.LookupCommandId('ID_SETTINGS_PARTITIONS')
-# postCmndID = RevitCommandId.LookupPostableCommandId(PostableCommand.Worksets)
-# print(postCmndID)
-# CmId = CmndID.Id
-# uiapp.PostCommand(postCmndID)
+        else:
+            # also refrshed
+            print("NO SUCH COMMAND")
--- GuoZhu_Beta.tab/Selection.panel/query.splitpushbutton/query.pushbutton/script.py
+++ GuoZhu_Beta.tab/Selection.panel/query.splitpushbutton/query.pushbutton/script.py
@@ -1,48 +1,68 @@
 # -*- coding: utf-8 -*-
 import clr
 import xml.etree.ElementTree as ET
-import os
-import shutil
-import json
 
 
 from pyrevit import forms, script
 
 from Autodesk.Revit.DB import*
 from Autodesk.Revit.UI.Selection import*
-from System.Collections.Generic import List
+
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
+def tunning(newRoot):
+    print("****"*10)
+    for rt in newRoot:
+        user = rt.get("name")
+        print("<{}>".format(user))
+        admin = rt.find("admin").text
+        print("\t admin-{}".format(admin))
+        workset = rt.find("workset").text
+        print("\t workset-{}".format(workset))
 
-current_username = __revit__.Application.Username
-#User input
-# username = os.environ.get('USERNAME')
 
+output = script.get_output()
 
 #Public config and local config
 public_config = "\\\\10.1.37.5\\国住共享文件夹\\国住设计区\\设计共享区\\BIM项目\\pyRevitExtension\\GUOZHU_Beta.extension\\GUOZHU_Beta_config.xml"
 text_XML = "C:\\Users\\6321011\\Desktop\\textXML.xml"
 
 
+# The main code check for admin
+try:
+    value = forms.ask_for_string(
+        
+        prompt='INPUT ADMIN CODE:',
+        title='Admin Check'
+    )
+
+    print(value)
+    if value == "1q2w3e4r":
+        print("correct")
+    else:
+        forms.alert("错误，请联系管理员")
+        output.close()
+        script.exit()
+except:
+    script.exit()
+
+
 # parse the public config(original) from the utf-8 xml
 tree = ET.parse(public_config)
 root = tree.getroot()
-
-
-
+# Create new root
 newRoot = ET.Element("data")
+
 
 for rt in root:
 
     user = rt.get("name").decode("utf-8")
-    print(user)
     admin = rt.find("admin").text
-    print(admin)
     workset = rt.find("workset").text
-    print(workset)
 
+    #Create new root
     newElem = ET.Element("user", name = user)
 
     subE1 = ET.SubElement(newElem, "admin")
@@ -54,4 +74,84 @@ for rt in root:
     newRoot.append(newElem)
 
 newTree = ET.ElementTree(newRoot)
-newTree.write(text_XML, encoding="UTF-8",xml_declaration=True)
+
+# running cmd command
+exit_condition = False
+while exit_condition == False:
+    #user input command
+    
+    while True:
+        tunning(newRoot)
+        users = [i.get("name") for i in newRoot.findall("user")]
+        cmd = raw_input()
+
+        if cmd.lower() == "rf":
+            exit_condition = True
+            break
+
+        #cmd for individual user
+        if len(cmd.split("_"))==2:
+
+            for user in newRoot.findall("user"):
+                input_name = cmd.split("_")[0]
+                input_cmd1 = cmd.split("_")[1]
+                
+                if user.get("name") == input_name:
+                    if input_cmd1.lower() == "workset.true" :
+                        user.find("workset").text = "True"
+
+                    elif input_cmd1.lower() == "workset.false":
+                        user.find("workset").text = "False"
+
+                    elif input_cmd1.lower() == "admin.true":
+                        user.find("admin").text = "True"
+
+                    elif input_cmd1.lower() == "admin.false":
+                        user.find("admin").text = "False"
+
+            exit_condition = False
+            break
+
+        if cmd.lower() == "admin.alltrue":
+            # set all user's admin to ture
+            for i in newRoot.iter("admin"):
+                i.text = "True"
+            exit_condition = False
+            break
+
+        if cmd.lower() == "admin.allfalse":
+            # set all user's admin to False
+            for i in newRoot.iter("admin"):
+                i.text = "False"
+            exit_condition = False
+            break
+
+        if cmd.lower() == "workset.alltrue":
+            # set all user's workset to ture
+            for i in newRoot.iter("workset"):
+                i.text = "True"
+            exit_condition = False
+            break
+
+        if cmd.lower() == "workset.allfalse":
+            # set all user's workset to false
+            for i in newRoot.iter("workset"):
+                i.text = "False"
+            exit_condition = False
+            break
+
+        if cmd.lower() == "exit":
+            # exit with nothing changed
+            exit_condition = True
+            output.close()
+            break
+
+        if cmd.lower() == "write":
+            # writ to xml and exit
+            newTree.write(public_config, encoding="UTF-8",xml_declaration=True)
+            exit_condition = True
+            break
+
+        else:
+            # also refrshed
+            print("NO SUCH COMMAND")
